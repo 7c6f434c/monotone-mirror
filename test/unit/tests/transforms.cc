9c9e81b1f4f5af7236577711e09651953f3d4546
markus <markus@bluegap.ch>
2017-11-07 19:19:58 +0000

    Add more `transforms` unit tests for better gzip coverage.

--- test/unit/tests/transforms.cc
+++ test/unit/tests/transforms.cc
@@ -1,4 +1,5 @@
 // Copyright (C) 2002 Graydon Hoare <graydon@pobox.com>
+//               2017 Markus Wanner <markus@bluegap.ch>
 //
 // This program is made available under the GNU GPL version 2.0 or
 // greater. See the accompanying file COPYING for details.
@@ -8,12 +9,81 @@
 // PURPOSE.
 
 #include "../../../src/base.hh"
+#include "../../../src/vector.hh"
+
 #include "../unit_tests.hh"
 #include "../../../src/transforms.hh"
 
 using std::string;
+using std::vector;
 
-UNIT_TEST(enc)
+UNIT_TEST(gzip_encode)
+{
+  vector<unsigned char> const
+    EXP_HDR = {0x1f, 0x8b, 0x08, 0x00},
+    EXP_CRC = {0x37, 0xf0, 0x29, 0xb6},
+    EXP_LEN = {0x11, 0, 0, 0};
+
+  gzip<data> gzd;
+  encode_gzip(data("the rain in spain"), gzd);
+
+  // gzip header and footer alone weights 18 bytes.
+  UNIT_TEST_CHECK(gzd().size() > 18);
+
+  // check the gzip header - first four bytes
+  string hdr = gzd().substr(0, 4);
+  UNIT_TEST_CHECK(hdr == string(EXP_HDR.begin(), EXP_HDR.end()));
+
+  if (gzd().size() > 8)
+    {
+      // check the trailor's CRC
+      string crc = gzd().substr(gzd().size() - 8, 4);
+      UNIT_TEST_CHECK(crc == string(EXP_CRC.begin(), EXP_CRC.end()));
+
+      // check the trailor's length info.
+      string len = gzd().substr(gzd().size() - 4);
+      UNIT_TEST_CHECK(len == string(EXP_LEN.begin(), EXP_LEN.end()));
+    }
+}
+
+UNIT_TEST(gzip_decode_old)
+{
+  // "the rain in spain" gzipped with monotone's old gzip logic, which
+  // doesn't set the timestamp and indicates a length of zero in the
+  // trailer.
+  base64< gzip<data> >
+    bgzd("H4sIAAAAAAAA/yvJSFUoSszMUwCi4gIgAwA38Cm2EQAAAA==");
+  gzip<data> gzd2;
+  gzd2 = decode_base64(bgzd);
+  data d2;
+  decode_gzip(gzd2, d2);
+  UNIT_TEST_CHECK(d2() == "the rain in spain");
+}
+
+UNIT_TEST(gzip_roundtrip)
+{
+  data d2, d1("the rain in spain");
+  gzip<data> gzd;
+  encode_gzip(d1, gzd);
+  decode_gzip(gzd, d2);
+  UNIT_TEST_CHECK(d2 == d1);
+}
+
+UNIT_TEST(base64_decode)
+{
+  base64<data> in("dGhlIHJhaW4gaW4gc3BhaW4=");
+  data out = decode_base64(in);
+  UNIT_TEST_CHECK(out() == "the rain in spain");
+}
+
+UNIT_TEST(base64_encode)
+{
+  data in("the rain in spain");
+  base64<data> out = encode_base64(in);
+  UNIT_TEST_CHECK(out() == "dGhlIHJhaW4gaW4gc3BhaW4=");
+}
+
+UNIT_TEST(gzip_base64_roundtrip)
 {
   data d2, d1("the rain in spain");
   gzip<data> gzd1, gzd2;
